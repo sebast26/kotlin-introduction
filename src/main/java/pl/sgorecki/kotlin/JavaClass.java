f98514f42977bd341a0084295c12c4bec0c03f07
Sebastian Górecki <gorecki.sebastian@gmail.com>
2018-04-25 22:59:06 +0200

    Kotlin and Java calling each other. Conversion between Int and int.

--- src/main/java/pl/sgorecki/kotlin/JavaClass.java
+++ src/main/java/pl/sgorecki/kotlin/JavaClass.java
@@ -9,5 +9,12 @@ class JavaClass {
     public static void main(String[] args) {
         final JavaClass javaClass = new JavaClass();
         javaClass.sayHello();
+
+        final KotlinClass kotlinClass = new KotlinClass();
+        kotlinClass.sayHello();
+        System.out.println(kotlinClass.getName());
+        final int age = kotlinClass.getAge();
+        System.out.println(age);
+        System.out.println(kotlinClass.getBirthday());
     }
 }
