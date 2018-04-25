22ab9c9be10a94bda8ce56d614d1acf8d016fda9
Sebastian Górecki <gorecki.sebastian@gmail.com>
2018-04-25 22:32:28 +0200

    Adds JavaClass with sayHello method.

---
+++ src/main/java/pl/sgorecki/kotlin/JavaClass.java
@@ -0,0 +1,12 @@
+package pl.sgorecki.kotlin;
+
+class JavaClass {
+
+    public void sayHello() {
+        System.out.println("Hello from Java class!");
+    }
+
+    public static void main(String[] args) {
+
+    }
+}
