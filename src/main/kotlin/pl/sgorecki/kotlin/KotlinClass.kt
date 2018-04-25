b2695dc76aefe1c8eb1f2b724e7524b085b24958
Sebastian Górecki <gorecki.sebastian@gmail.com>
2018-04-25 23:44:57 +0200

    Adds missing KotlinClass impl.

--- src/main/kotlin/pl/sgorecki/kotlin/KotlinClass.kt
+++ src/main/kotlin/pl/sgorecki/kotlin/KotlinClass.kt
@@ -1,4 +1,20 @@
 package pl.sgorecki.kotlin
 
+import java.time.LocalDate
+
 class KotlinClass {
+    val name: String = "Seba"
+    val age: Int = 34
+    val birthday: LocalDate = LocalDate.of(1984, 2, 18)
+
+    fun sayHello() {
+        println("Hello from Kotlin class!")
+    }
+}
+
+fun main(args: Array<String>) {
+    val javaClass = JavaClass()
+    javaClass.sayHello()
+    val kotlinClass = KotlinClass()
+    kotlinClass.sayHello()
 }
