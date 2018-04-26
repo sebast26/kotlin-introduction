ec9e53bee33eec8b2bf6cc6489c75e373aec23c3
Sebastian Górecki <gorecki.sebastian@gmail.com>
2018-04-26 19:42:33 +0200

    Example of NPE safety and safe calls with ?.

--- src/main/kotlin/pl/sgorecki/kotlin/02NpeSafety.kt
+++ src/main/kotlin/pl/sgorecki/kotlin/02NpeSafety.kt
@@ -1,2 +1,26 @@
 package pl.sgorecki.kotlin
 
+val firstName: String = "Seba"
+val secondName: String? = null
+
+var address: String = "Poznan"
+fun tryToNullify() {
+//    address = null
+}
+
+class PersonalInfo(val email: String?)
+class Client(val personalInfo: PersonalInfo?)
+
+fun getEmailOrNull(client: Client?): String? {
+//    if (client == null) {
+//        return null
+//    }
+//    if (client.personalInfo == null) {
+//        return null
+//    }
+//    if (client.personalInfo.email == null) {
+//        return null
+//    }
+//    return client.personalInfo.email
+    return client?.personalInfo?.email
+}
