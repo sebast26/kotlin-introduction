b7d27d8711e35fa679f8177d005659a976f02f69
Sebastian Górecki <gorecki.sebastian@gmail.com>
2018-04-26 19:34:53 +0200

    Adds NPE var example.

--- src/main/kotlin/pl/sgorecki/kotlin/01NpeSafety.kt
+++ src/main/kotlin/pl/sgorecki/kotlin/01NpeSafety.kt
@@ -3,6 +3,11 @@ package pl.sgorecki.kotlin
 val firstName: String = "always not null"
 val secondName: String? = null
 
+var address: String = "Poznan"
+fun tryToNullify() {
+//    address = null
+}
+
 class PersonalInfo(val email: String?)
 class Client(val personalInfo: PersonalInfo?)
 
