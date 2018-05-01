2724428882a6d02f5be9dd490d3750fc2e9da06b
Sebastian Górecki <gorecki.sebastian@gmail.com>
2018-05-01 19:45:43 +0200

    Template to show collections methods.

---
+++ src/main/kotlin/pl/sgorecki/kotlin/06Collections.kt
@@ -0,0 +1,19 @@
+package pl.sgorecki.kotlin
+
+fun main(args: Array<String>) {
+
+    val max = listOf(2, 42, 4)
+    println(max)
+
+//    val maxBy = listOf("a", "abcd", "aa", "dd", "abc", "xyz")
+//    println(maxBy)
+
+//    val sortedBy = listOf("a", "abcd", "aa", "dd", "abc", "xyz")
+//    println(sortedBy)
+
+//    val groupBy = listOf("a", "abcd", "aa", "dd", "abc", "xyz")
+//    println(groupBy)
+
+    val numbers = listOf(-1, 42, -30, 12, 155)
+
+}
